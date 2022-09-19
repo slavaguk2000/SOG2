4a783f8dcbed9f842cd6475f153b3ac2485ad6bc
viacheslav <viacheslav.g@5ly.co>
2022-09-19 05:24:40 +0300

    fix cors errors

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from ariadne import make_executable_schema, snake_case_fallback_resolvers
 from ariadne.asgi import GraphQL
+from starlette.middleware.cors import CORSMiddleware
 
 from src.services.elasticsearch.elastic import Elastic
 from src.typedefs.main_typedefs import type_defs
@@ -8,4 +9,4 @@ from src.resolvers.resolvers import resolvers
 print("aaa")
 
 schema = make_executable_schema(type_defs, resolvers, snake_case_fallback_resolvers)
-app = GraphQL(schema, debug=True)
+app = CORSMiddleware(GraphQL(schema, debug=True), allow_origins=['*'], allow_methods=("GET", "POST", "OPTIONS"))
