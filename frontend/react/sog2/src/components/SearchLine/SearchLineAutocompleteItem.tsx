5ce632ad39a0c6fe8c470aefae57a98f36b87f93
Viacheslav Guk <33941934+slavaguk2000@users.noreply.github.com>
2023-09-17 05:52:19 +0300

    add_location_in_autocomplete (#5)
    Co-authored-by: viacheslav <viacheslav.g@5ly.co>

--- frontend/react/sog2/src/components/SearchLine/SearchLineAutocompleteItem.tsx
+++ frontend/react/sog2/src/components/SearchLine/SearchLineAutocompleteItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { useBibleData } from '../../providers/bibleDataProvider';
 import { Slide } from '../../utils/gql/types';
 
 import { SearchLineAutocompleteItemWrapper } from './styled';
@@ -12,14 +13,24 @@ interface SearchLineAutocompleteItemProps {
 }
 
 const SearchLineAutocompleteItem = ({
-  slide: { content },
+  slide: { content, location },
   onClick,
   selected,
   setSelected,
 }: SearchLineAutocompleteItemProps) => {
+  const { bibleBooksData } = useBibleData();
+
+  const locationLength = location.length;
+
+  const bookIdx = Number(location[locationLength - 3]);
+
+  const book = bibleBooksData && bookIdx !== undefined ? bibleBooksData[bookIdx] : undefined;
+
+  const lineText = `${book?.name} ${location[locationLength - 2]}:${location[locationLength - 1]} ${content}`;
+
   return (
     <SearchLineAutocompleteItemWrapper onClick={onClick} selected={selected} onMouseEnter={setSelected}>
-      {content}
+      {lineText}
     </SearchLineAutocompleteItemWrapper>
   );
 };
