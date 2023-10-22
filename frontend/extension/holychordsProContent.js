76b2c2cfc74df214e44babcfc6a19e513d368156
Viacheslav <slavaguk2000@gmail.com>
2023-10-22 12:49:25 +0300

    add showing all song with chords from holychords

--- frontend/extension/holychordsProContent.js
+++ frontend/extension/holychordsProContent.js
@@ -4,38 +4,60 @@ const showSlide = (text, title) => {
             console.error(response);
         }
     });
-}
+};
+
+const getCouplets = () => document.querySelectorAll('div.blocks');
+
+const addButtonInChildren = (el, listner, up) => {
+    const button = document.createElement('img');
+
+    button.src = chrome.runtime.getURL("icon128.png");
+
+    button.style.marginLeft = '5px';
+    button.style.cursor = 'pointer';
+    button.style.position = 'absolute';
+    button.style.top = '0';
+    if (!up) {
+        button.style.left= '-20px';    
+    }
+    button.style.height = '30px';
+    button.style.background = 'powderblue';
+    button.style.borderRadius = '50%';
+    button.style.padding = '3px';
+    el.style.position = 'relative';
+
+    button.addEventListener('click', listner);
+
+    el.insertBefore(button, el.children[0]);
+};
+
+const getTextFromCouplet = (el, withChord) => {
+    const coupletNumber = el.querySelector('b').textContent.match(/\d+/)?.[0];
+    const coupletText = Array.from(el.querySelectorAll(withChord ? 'div>span' : 'div>span.text')).map(({ textContent }) => textContent.trim()).join('\n');
+    return (coupletNumber ? coupletNumber.trim() + '. ' : '* ') + coupletText.trim();
+};
+
+const getSlideTitle = () => {
+    return document.querySelector('h2').textContent.trim();
+};
 
 const addIconsToCouplets = () => {
-    const divs = document.querySelectorAll('div.blocks');
-
-    divs.forEach(div => {
-        const button = document.createElement('img');
-
-        button.src = chrome.runtime.getURL("icon128.png");
-
-        button.style.marginLeft = '5px';
-        button.style.cursor = 'pointer';
-        button.style.position = 'absolute';
-        button.style.top = '0';
-        button.style.left= '-20px';
-        button.style.height = '30px';
-        button.style.background = 'powderblue';
-        button.style.borderRadius = '50%';
-        button.style.padding = '3px';
-        div.style.position = 'relative';
-
-        button.addEventListener('click', () => {
-            const coupletNumber = div.querySelector('b').textContent.match(/\d+/)?.[0];
-            const coupletText = Array.from(div.querySelectorAll('span.text')).map(({ textContent }) => textContent.trim()).join('\n');
-            const songTitle = document.querySelector('h2').textContent;
-            const slideText = (coupletNumber ? coupletNumber.trim() + '. ' : '* ') + coupletText;
-            const slideTitle = songTitle.trim();
-            showSlide(slideText, slideTitle);
-        });
-
-        div.insertBefore(button, div.children[0]);
+    getCouplets().forEach(div => {
+        addButtonInChildren(div, () => {
+                const slideText = getTextFromCouplet(div);
+                showSlide(slideText, getSlideTitle());
+            }
+        )
     });
-}
+};
 
-addIconsToCouplets()
+const addIconToTitle = () => {
+    const header = document.querySelector('h2.t-worship-leader__marquee__headline.mb-0');
+    addButtonInChildren(header, () => {
+        const slideText = Array.from(getCouplets()).map((div) => getTextFromCouplet(div, true)).join('\n\n');
+        showSlide(slideText, getSlideTitle());
+    }, true);
+};
+
+addIconsToCouplets();
+addIconToTitle();
