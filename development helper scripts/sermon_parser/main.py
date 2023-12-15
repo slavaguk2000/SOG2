e0ff3ca2b01a63461db3ec846fd81443b3a65c81
viacheslav <viacheslav.g@5ly.co>
2023-12-15 07:41:50 +0300

    add possibility to add sermon data to elastic

--- development helper scripts/sermon_parser/main.py
+++ development helper scripts/sermon_parser/main.py
@@ -1,10 +1,22 @@
 import httpx
+import uuid
 import re
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
+from elasticSearchServise.ElasticSearch import Elastic
+from elasticSearchServise.mappings import sermon_mapping
+
 base_url = 'https://branham.ru'
 main_path = 'sermons'
+
+
+def flatten_deeply_nested_list(nested_list):
+    for item in nested_list:
+        if isinstance(item, list):
+            yield from flatten_deeply_nested_list(item)
+        else:
+            yield item
 
 
 def get_text_content_for_sog(sermon_data):
@@ -19,7 +31,7 @@ def get_text_content_for_sog(sermon_data):
 
 def get_text_file_for_sog(sermons_data_array):
     return '\n'.join([
-        f"{sermon_data['year']}\n({sermon_data['translation']}) {sermon_data['title']}\n" +
+        f"{sermon_data['year']}\n({sermon_data['translation']}) {sermon_data['title'].upper()}\n" +
         f"{get_text_content_for_sog(sermon_data)}\n" for sermon_data in sermons_data_array
     ])
 
@@ -33,7 +45,7 @@ def parse_current_sermon(url):
             soup = BeautifulSoup(response.text, 'html.parser')
 
             year = re.sub('\\s', '', soup.select('span.my-1')[0].get_text().replace('-', '').strip())
-            title = soup.select('h4.mt-3')[0].get_text().strip().upper()
+            title = soup.select('h4.mt-3')[0].get_text().strip()
             translation = soup.select('span.my-1')[2].get_text().strip()
 
             content = [
@@ -53,12 +65,8 @@ def parse_current_sermon(url):
                     if (hq_audio_link is not None) or (not len(audio_links)) else \
                     audio_links[0]
 
-            print(year.strip())
-            print(title.strip())
-            print(translation.strip())
-            print(result_audio_link.strip() if result_audio_link else result_audio_link)
-            # print(content)
-            return { "year": year, "title": title, "translation": translation, "content": content, "audio_link": result_audio_link.strip() if result_audio_link else result_audio_link }
+            return {"year": year, "title": title, "translation": translation, "content": content,
+                    "audio_link": result_audio_link.strip() if result_audio_link else result_audio_link}
         except BaseException as e:
             print(e)
 
@@ -79,7 +87,7 @@ def scrape_branham_sermons():
         future_to_sermon = {}
 
         with ThreadPoolExecutor() as executor:
-            for row in sermon_rows:
+            for row in sermon_rows[0:10]:
                 sermon_link = row.find('a', class_='font-weight-bold')
                 if sermon_link and sermon_link.text:
                     link = sermon_link['href']
@@ -98,6 +106,46 @@ def scrape_branham_sermons():
         print('Не удалось получить данные с сайта')
 
 
+def convert_sermon_year_to_datetime(date_str):
+    time_mapping = {'M': '10:00', 'A': '14:00', 'E': '18:00', '': '16:00'}
+    match = re.match(r'(\d{2})(\d{2})(\d{2})([MAE]?)', date_str)
+    if not match:
+        raise ValueError(f"Incorect date format: {date_str}")
+
+    year, month, day, time_suffix = match.groups()
+    year = f"19{year}"
+
+    time = time_mapping[time_suffix]
+
+    return f"{year}-{month}-{day}T{time}:00"
+
+
+def get_elastic_data_from_single_sermon_data(sermon_data):
+    sermon_date = convert_sermon_year_to_datetime(sermon_data["year"])
+
+    return [[{
+        "_id": str(uuid.uuid4()),
+        "sermon_collection_id": "0",
+        "sermon_name": sermon_data["title"],
+        "sermon_translation": sermon_data["translation"],
+        "sermon_date": sermon_date,
+        "audio_link": sermon_data["audio_link"],
+        **({} if idx else {"chapter": chapter["chapter"]}),
+        "chapter_content": paragraph,
+    } for idx, paragraph in enumerate(chapter['paragraphs'])] for chapter in sermon_data["content"]]
+
+
+def add_sermon_data_to_elastic(sermons_data):
+    elastic_bulk_data_from_branham_ru = list(flatten_deeply_nested_list([
+        get_elastic_data_from_single_sermon_data(sermon_data) for sermon_data in branham_sermons_data
+    ]))
+    el = Elastic()
+    el.clear_index(sermon_mapping.index)
+    el.bulk_create(elastic_bulk_data_from_branham_ru, sermon_mapping.index)
+
+
 branham_sermons_data = scrape_branham_sermons()
-with open('branham-ru.sog', mode='w', encoding='utf-8') as f:
-    f.write(get_text_file_for_sog(branham_sermons_data))
+add_sermon_data_to_elastic(branham_sermons_data)
+
+
+
