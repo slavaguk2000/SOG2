dc06e66e40ad452695d15ef404a62313f319d48d
Viacheslav <slavaguk2000@gmail.com>
2024-03-12 17:35:11 +0300

    fix mappings

--- backend/install.sh
+++ backend/install.sh
@@ -1,2 +1,2 @@
 python3 -m venv venv
-venv/bin/python3 -m pip install ariadne starlette elasticsearch singleton_decorator "uvicorn[standard]" sqlalchemy alembic
+venv/bin/python3 -m pip install ariadne starlette elasticsearch singleton_decorator "uvicorn[standard]" sqlalchemy alembic regex
