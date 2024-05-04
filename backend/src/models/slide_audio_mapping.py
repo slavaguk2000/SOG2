667d16be2a5f0cadd8386f42f5bf84726db99ff1
viacheslav <viacheslav.g@5ly.co>
2024-05-05 00:07:26 +0200

    add space offset

--- backend/src/models/slide_audio_mapping.py
+++ backend/src/models/slide_audio_mapping.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Column, String, ForeignKey, Integer
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 from src.services.database import Base
 
@@ -12,6 +12,7 @@ class SlideAudioMapping(Base):
     slide_collection_audio_mapping_id = Column(String(36), ForeignKey('sermon_audio_mappings.id'), nullable=False)
     time_point = Column(Integer, nullable=False)
     chars_offset = Column(Integer, nullable=False, server_default='0')
+    space_offset = Column(Float, nullable=False, server_default='0')
     slide_id = Column(String(36), ForeignKey('paragraphs.id'), nullable=False)
 
     sermon_audio_mapping = relationship("SermonAudioMapping", back_populates="slide_audio_mappings")
