073b93d43b3abcc1ab72502a6795c1d7918b1f7b
viacheslav <viacheslav.g@5ly.co>
2024-05-07 16:58:42 +0200

    add blink to record button

--- frontend/react/sog2/src/providers/AudioMapping/controller.tsx
+++ frontend/react/sog2/src/providers/AudioMapping/controller.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 import LocationDisabledIcon from '@mui/icons-material/LocationDisabled';
 import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
-import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import { Button, Tooltip } from '@mui/material';
 
 import { useAudioMapping } from './provider';
-import { AudioMappingControllerWrapper } from './styled';
+import { AudioMappingControllerWrapper, BlinkedRadioButtonCheckedIcon } from './styled';
 
 const AudioMappingController = () => {
   const { recording, setRecording, follow, setFollow } = useAudioMapping();
@@ -20,7 +19,7 @@ const AudioMappingController = () => {
       </Tooltip>
       <Tooltip title={'Record text time mapping'}>
         <Button onClick={() => setRecording((p) => !p)}>
-          <RadioButtonCheckedIcon color={recording ? 'error' : undefined} />
+          <BlinkedRadioButtonCheckedIcon color={recording ? 'error' : undefined} />
         </Button>
       </Tooltip>
     </AudioMappingControllerWrapper>
