80aba4ebc382c6de2912ffe748177daaff702a9c
Viacheslav Guk <33941934+slavaguk2000@users.noreply.github.com>
2024-05-07 16:13:52 +0200

    update install scripts (#46)
    Co-authored-by: viacheslav <viacheslav.g@5ly.co>

--- backend/install.bat
+++ backend/install.bat
@@ -1,2 +1,3 @@
 python -m venv venv
 venv\Scripts\python.exe -m pip install ariadne starlette elasticsearch singleton_decorator "uvicorn[standard]" sqlalchemy alembic regex
+venv\Scripts\python.exe -m  alembic upgrade head
