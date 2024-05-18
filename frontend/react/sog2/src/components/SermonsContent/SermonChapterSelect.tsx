05229976f8a7d5343a630422f33b07620d2da10a
viacheslav <viacheslav.g@5ly.co>
2024-05-18 02:14:09 +0200

    add chapter search features

--- frontend/react/sog2/src/components/SermonsContent/SermonChapterSelect.tsx
+++ frontend/react/sog2/src/components/SermonsContent/SermonChapterSelect.tsx
@@ -1,17 +1,23 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 
+import PreselectBox from '../../hooks/useFastNumberSelection/PreselectBox';
+import useFastNumberSelection from '../../hooks/useFastNumberSelection/useFastNumberSelection';
 import { useInstrumentsField } from '../../providers/instrumentsFieldProvider';
 import { usePlayerContext } from '../../providers/playerProvider';
 import { useSermonData } from '../../providers/sermanDataProvider';
 import { formatTime } from '../../utils';
+import { Slide } from '../../utils/gql/types';
 import SlideWithPreview from '../SlideWithPreview';
 
 import { SermonChapterSelectWrapper } from './styled';
+
+const debounceSeconds = 0.7;
 
 const SermonChapterSelect = () => {
   const { currentSermonSlides, handleUpdateSlide, audioMapping } = useSermonData();
   const { currentSlide } = useInstrumentsField();
   const { isPlaying } = usePlayerContext();
+  const chaptersRef = useRef<HTMLElement>(null);
 
   const preparedData = useMemo(
     () =>
@@ -30,8 +36,64 @@ const SermonChapterSelect = () => {
     [audioMapping?.id, currentSermonSlides, isPlaying],
   );
 
+  const { numberToSlideMap, maxSlideNumber } = useMemo(() => {
+    if (!preparedData) {
+      return {};
+    }
+
+    let maxNumber = 0;
+    const serviceNumberToSlideMap: Record<number, { slide: Slide; idx: number }> = {};
+    const resultNumberToSlideMap: Record<number, Slide> = {};
+
+    preparedData.forEach(({ slide }, idx) => {
+      if (slide.location?.[2]) {
+        const slideNumber = Number(slide.location[2]);
+        serviceNumberToSlideMap[slideNumber] = { slide, idx };
+        maxNumber = Math.max(slideNumber, maxNumber);
+      }
+    });
+
+    for (let i = 1; i <= maxNumber; i++) {
+      if (!serviceNumberToSlideMap[i]) {
+        const prevIdx = serviceNumberToSlideMap[i - 1]?.idx;
+        if (prevIdx) {
+          const currentIdx = prevIdx + 1;
+          serviceNumberToSlideMap[i] = {
+            slide: preparedData[currentIdx].slide,
+            idx: currentIdx,
+          };
+        }
+      }
+
+      resultNumberToSlideMap[i] = serviceNumberToSlideMap[i].slide;
+    }
+
+    return {
+      numberToSlideMap: resultNumberToSlideMap,
+      maxSlideNumber: maxNumber,
+    };
+  }, [preparedData]);
+
+  const changeChapterByNumber = (requestedNumber: number) => {
+    const requestedSlide = numberToSlideMap?.[requestedNumber];
+    if (requestedSlide) {
+      handleUpdateSlide(requestedSlide);
+    }
+  };
+
+  const { preselectNumber, handleKeyDown } = useFastNumberSelection(changeChapterByNumber, Number(maxSlideNumber), {
+    debounceSeconds,
+  });
+
+  useEffect(() => {
+    if (currentSlide) {
+      chaptersRef?.current?.focus();
+    }
+  }, [currentSlide]);
+
   return (
-    <SermonChapterSelectWrapper>
+    <SermonChapterSelectWrapper tabIndex={0} onKeyDown={handleKeyDown} ref={chaptersRef}>
+      <PreselectBox preselectNumber={preselectNumber} debounceSeconds={debounceSeconds} />
       {preparedData?.map(({ content, id, slide, tooltip }) => (
         <SlideWithPreview
           key={id}
---
+++ frontend/react/sog2/src/hooks/useFastNumberSelection/useFastNumberSelection.ts
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+
+const useFastNumberSelection = (
+  changeSlideByNumber: (currentNumber: number) => void,
+  maxLength: number,
+  {
+    debounceSeconds = 1,
+  }: {
+    debounceSeconds?: number;
+  } = {},
+): {
+  preselectNumber: number;
+  handleKeyDown: (event: React.KeyboardEvent) => void;
+} => {
+  const [preselectNumber, setPreselectNumber] = useState<number>(0);
+
+  useEffect(() => {
+    const selectPreselectNumberVerseCallback = setTimeout(() => {
+      if (preselectNumber) {
+        changeSlideByNumber(preselectNumber);
+
+        setPreselectNumber(0);
+      }
+    }, 1000 * debounceSeconds);
+
+    return () => clearTimeout(selectPreselectNumberVerseCallback);
+  }, [changeSlideByNumber, preselectNumber, setPreselectNumber]);
+
+  const handleNumberPressed = (pressedNumber: number): boolean => {
+    if (!maxLength || pressedNumber > maxLength) {
+      return false;
+    }
+
+    setPreselectNumber((prev) => {
+      const maybeNewPreselect = prev * 10 + pressedNumber;
+
+      if (maybeNewPreselect * 10 > maxLength) {
+        changeSlideByNumber(maybeNewPreselect > maxLength ? prev : maybeNewPreselect);
+
+        return 0;
+      }
+
+      return maybeNewPreselect;
+    });
+
+    return true;
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key.length === 1 && /[0-9]/.test(event.key)) {
+      if (handleNumberPressed(Number(event.key))) {
+        event.stopPropagation();
+      }
+    }
+  };
+
+  return {
+    preselectNumber,
+    handleKeyDown,
+  };
+};
+
+export default useFastNumberSelection;
