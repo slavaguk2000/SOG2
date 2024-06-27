7e61376428d87ba5f08f759cc584fec867b79a23
viacheslav <viacheslav.g@5ly.co>
2024-06-27 02:15:22 +0200

    improve duplicate chord corner case

--- frontend/react/sog2/src/components/psalmChords/editableChordsDataProvider/implementGlueWithNextLine.ts
+++ frontend/react/sog2/src/components/psalmChords/editableChordsDataProvider/implementGlueWithNextLine.ts
@@ -15,10 +15,18 @@ const implementGlueWithNextLine = ({ setNewChordsData, chordsData }: ImplementGl
           return {
             ...couplet,
             coupletContent: couplet.coupletContent.reduce((acc: CoupletContent[], coupletContentItem) => {
-              acc.push({
-                ...coupletContentItem,
-                line: coupletContentItem.line > lineNumber ? coupletContentItem.line - 1 : coupletContentItem.line,
-              });
+              if (
+                acc.length &&
+                coupletContentItem.line === lineNumber + 1 &&
+                acc[acc.length - 1].chord.id === coupletContentItem.chord.id
+              ) {
+                acc[acc.length - 1].text = `${acc[acc.length - 1].text}${coupletContentItem.text}`;
+              } else {
+                acc.push({
+                  ...coupletContentItem,
+                  line: coupletContentItem.line > lineNumber ? coupletContentItem.line - 1 : coupletContentItem.line,
+                });
+              }
 
               return acc;
             }, []),
