00cd7f0c4ec0edf89a408f852b62b8ab0b508548
viacheslav <viacheslav.g@5ly.co>
2024-06-28 23:45:38 +0200

    add possibility to get static psalm images

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,35 @@
 from ariadne import make_executable_schema, snake_case_fallback_resolvers
 from ariadne.asgi import GraphQL
+from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
+from starlette.routing import Mount
+from starlette.staticfiles import StaticFiles
 
 from src.typedefs.main_typedefs import type_defs
 from src.resolvers.resolvers import resolvers
 
 
+IMAGE_DIRECTORY = "psalms_images/songs_books"
+
 schema = make_executable_schema(type_defs, resolvers, snake_case_fallback_resolvers)
-app = CORSMiddleware(GraphQL(schema, debug=True), allow_origins=['*'], allow_methods=("GET", "POST", "OPTIONS"))
+graphql_app = GraphQL(schema, debug=True)
+
+
+routes = [
+    Mount("/", graphql_app, name="/"),
+    Mount("/psalm-image", StaticFiles(directory=IMAGE_DIRECTORY), name="static")
+]
+
+app = Starlette(routes=routes)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- backend/start_dev.sh
+++ backend/start_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source venv/bin/activate
-python3 -m uvicorn main:app --host 0.0.0.0 --reload
+python3 -m uvicorn main:app --host 0.0.0.0 --reload --log-level debug
