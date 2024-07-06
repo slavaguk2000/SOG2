6055d1837fcb36a48b9e1b7d4913dc8ef6359caf
viacheslav <viacheslav.g@5ly.co>
2024-07-06 03:19:27 +0200

    fix list speed

--- frontend/react/sog2/src/components/PsalmsContent/mobile/PsalmsList.tsx
+++ frontend/react/sog2/src/components/PsalmsContent/mobile/PsalmsList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { FixedSizeList, ListChildComponentProps } from 'react-window';
+import { FixedSizeList } from 'react-window';
 
-import { IconButton, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { ListItem, ListItemButton, ListItemText } from '@mui/material';
 import { useDebouncedCallback } from 'use-debounce';
 
 import { usePsalmsData } from '../../../providers/dataProviders/psalmsDataProvider';
@@ -11,7 +11,6 @@ import { PsalmsListWrapper } from './styled';
 
 const PsalmsList = () => {
   const { psalmsData } = usePsalmsData();
-  const [checked, setChecked] = useState<string[]>([]);
   const [height, setHeight] = useState<number | null>(null);
   const ref = useRef<HTMLDivElement | null>(null);
 
@@ -20,60 +19,43 @@ const PsalmsList = () => {
   }, 5000);
 
   useEffect(() => {
-    if (ref?.current?.clientHeight) {
-      debouncedSetHeight(ref.current.clientHeight);
+    const clientHeight = ref?.current?.clientHeight;
+    if (clientHeight) {
+      if (height) {
+        debouncedSetHeight(clientHeight);
+      } else {
+        setHeight(clientHeight);
+      }
     }
   }, [debouncedSetHeight, ref]);
-
-  const handleToggle = (value: string) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
-  // TODO : fix click animation
-  // https://mui.com/material-ui/react-list/#virtualized-list
-  // https://github.com/bvaughn/react-virtualized/issues/657#issuecomment-381518076
-  const renderRow = ({ index, style }: ListChildComponentProps) => {
-    if (!psalmsData) return null;
-
-    const currentPsalm = psalmsData[index];
-
-    return (
-      <ListItem
-        sx={{ background: currentPsalm.inFavourite ? '#0253' : undefined }}
-        style={style}
-        key={currentPsalm.id}
-        secondaryAction={
-          <IconButton edge="end">
-            <InFavouriteIconButton
-              psalmId={currentPsalm.id}
-              inFavourite={currentPsalm.inFavourite}
-              transposition={currentPsalm.transposition}
-            />
-          </IconButton>
-        }
-        disablePadding
-      >
-        <ListItemButton role={undefined} onClick={handleToggle(currentPsalm.id)} dense>
-          <ListItemText primary={`${currentPsalm.psalmNumber} ${currentPsalm.name}`} />
-        </ListItemButton>
-      </ListItem>
-    );
-  };
 
   return (
     <PsalmsListWrapper ref={ref}>
       {psalmsData && height && (
         <FixedSizeList height={height} width={700} itemSize={46} itemCount={psalmsData.length} overscanCount={5}>
-          {renderRow}
+          {({ index, style }) => {
+            const currentPsalm = psalmsData[index];
+
+            return (
+              <ListItem
+                sx={{ background: currentPsalm.inFavourite ? '#0253' : undefined, height: '100%', width: '100%' }}
+                disablePadding
+                key={currentPsalm.id}
+                style={style}
+                secondaryAction={
+                  <InFavouriteIconButton
+                    psalmId={currentPsalm.id}
+                    inFavourite={currentPsalm.inFavourite}
+                    transposition={currentPsalm.transposition}
+                  />
+                }
+              >
+                <ListItemButton sx={{ height: '100%', width: '100%' }}>
+                  <ListItemText primary={`${currentPsalm.psalmNumber} ${currentPsalm.name}`} />
+                </ListItemButton>
+              </ListItem>
+            );
+          }}
         </FixedSizeList>
       )}
     </PsalmsListWrapper>
