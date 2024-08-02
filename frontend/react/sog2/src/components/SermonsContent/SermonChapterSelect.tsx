aab66b3a7da39c0378c2f225880c8b20e9c373ec
Viacheslav Guk <33941934+slavaguk2000@users.noreply.github.com>
2024-08-02 21:37:39 +0200

    Some audio player front impovements (#78)
    * change show times condition
    
    * add changing played by clicking to paragraph
    
    * add scrolling player behavior
    
    ---------
    
    Co-authored-by: viacheslav <viacheslav.g@5ly.co>

--- frontend/react/sog2/src/components/SermonsContent/SermonChapterSelect.tsx
+++ frontend/react/sog2/src/components/SermonsContent/SermonChapterSelect.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useMemo, useRef } from 'react';
 
 import PreselectBox from '../../hooks/useFastNumberSelection/PreselectBox';
 import useFastNumberSelection from '../../hooks/useFastNumberSelection/useFastNumberSelection';
+import { useAudioMapping } from '../../providers/AudioMapping/provider';
 import { useSermonData } from '../../providers/dataProviders/sermanDataProvider';
 import { useInstrumentsField } from '../../providers/instrumentsFieldProvider';
 import { usePlayerContext } from '../../providers/playerProvider';
@@ -16,7 +17,8 @@ const debounceSeconds = 0.7;
 const SermonChapterSelect = () => {
   const { currentSermonSlides, handleUpdateSlide, audioMapping } = useSermonData();
   const { currentSlide } = useInstrumentsField();
-  const { isPlaying } = usePlayerContext();
+  const { openInterface, seek } = usePlayerContext();
+  const { follow } = useAudioMapping();
   const chaptersRef = useRef<HTMLElement>(null);
 
   const preparedData = useMemo(
@@ -30,10 +32,11 @@ const SermonChapterSelect = () => {
           id: slide.id,
           content: `${slide.location?.[2] ? `${slide.location?.[2]}. ` : ''}${slide.content}`,
           slide,
-          tooltip: isPlaying && slideAudioMapping?.timePoint ? formatTime(slideAudioMapping.timePoint) : undefined,
+          tooltip: openInterface && slideAudioMapping?.timePoint ? formatTime(slideAudioMapping.timePoint) : undefined,
+          timePoint: slideAudioMapping?.timePoint,
         };
       }),
-    [audioMapping?.id, currentSermonSlides, isPlaying],
+    [audioMapping?.id, currentSermonSlides, openInterface],
   );
 
   const { numberToSlideMap, maxSlideNumber } = useMemo(() => {
@@ -74,10 +77,18 @@ const SermonChapterSelect = () => {
     };
   }, [preparedData]);
 
+  const onUpdateSlide = (newSlide?: Slide, timePoint?: number) => {
+    handleUpdateSlide(newSlide);
+
+    if (follow && timePoint !== undefined) {
+      seek(timePoint);
+    }
+  };
+
   const changeChapterByNumber = (requestedNumber: number) => {
     const requestedSlide = numberToSlideMap?.[requestedNumber];
     if (requestedSlide) {
-      handleUpdateSlide(requestedSlide);
+      onUpdateSlide(requestedSlide);
     }
   };
 
@@ -94,12 +105,12 @@ const SermonChapterSelect = () => {
   return (
     <SermonChapterSelectWrapper tabIndex={0} onKeyDown={handleKeyDown} ref={chaptersRef}>
       <PreselectBox preselectNumber={preselectNumber} debounceSeconds={debounceSeconds} />
-      {preparedData?.map(({ content, id, slide, tooltip }) => (
+      {preparedData?.map(({ content, id, slide, tooltip, timePoint }) => (
         <SlideWithPreview
           key={id}
           bibleEntityItemProps={{
             name: content,
-            onClick: () => handleUpdateSlide(slide),
+            onClick: () => onUpdateSlide(slide, timePoint ?? undefined),
             selected: currentSlide?.id === id,
             tooltip,
             scrollingOrder: 0,
