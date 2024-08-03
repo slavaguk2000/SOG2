d213dd96b7554e94836ab25233b6e751837dde61
viacheslav <viacheslav.g@5ly.co>
2024-08-03 03:26:20 +0200

    move logic into separate function

--- frontend/react/sog2/src/components/PsalmsContent/desktop/CoupletSelect.tsx
+++ frontend/react/sog2/src/components/PsalmsContent/desktop/CoupletSelect.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useMemo, useRef } from 'react';
 
+import { Maybe } from 'graphql/jsutils/Maybe';
+
 import PreselectBox from '../../../hooks/useFastNumberSelection/PreselectBox';
 import useFastNumberSelection from '../../../hooks/useFastNumberSelection/useFastNumberSelection';
 import {
@@ -7,12 +9,52 @@ import {
   useCurrentPsalms,
 } from '../../../providers/dataProviders/psalmsDataProvider/CurrentPsalmProvider';
 import { useInstrumentsField } from '../../../providers/instrumentsFieldProvider';
-import { Slide } from '../../../utils/gql/types';
+import { PsalmDataWithSlides, Slide } from '../../../utils/gql/types';
 import BibleEntityItem from '../../BibleContent/BibleEntityItem';
 
 import { CoupletSelectWrapper } from './styled';
 
 const debounceSeconds = 0.7;
+
+const getNecessaryNonNumericSlide = (
+  nonNumericCouplets: Record<number, Slide>,
+  psalmData: PsalmDataWithSlides,
+  currentSlideId?: Maybe<string>,
+) => {
+  const nonNumericCoupletsOrders = Object.keys(nonNumericCouplets).map(Number);
+
+  if (nonNumericCoupletsOrders.length === 1) {
+    return nonNumericCouplets[nonNumericCoupletsOrders[0]];
+  } else {
+    const currentCoupletOrder = psalmData.couplets.find(({ slide }) => slide?.id === currentSlideId)?.couplet
+      .initialOrder;
+
+    if (currentCoupletOrder !== undefined) {
+      const currentNonNumericIndex = nonNumericCoupletsOrders.indexOf(currentCoupletOrder);
+
+      if (currentNonNumericIndex === -1) {
+        const nextCoupletOrder = currentCoupletOrder + 1;
+        if (nonNumericCoupletsOrders.includes(nextCoupletOrder)) {
+          return nonNumericCouplets[nextCoupletOrder];
+        } else {
+          const previousOrders = nonNumericCoupletsOrders.filter((i) => i < currentCoupletOrder);
+
+          if (previousOrders.length) {
+            return nonNumericCouplets[previousOrders[previousOrders.length - 1]];
+          } else {
+            return nonNumericCouplets[nonNumericCoupletsOrders[0]];
+          }
+        }
+      } else {
+        const nextOrderIndex =
+          currentNonNumericIndex + 1 < nonNumericCoupletsOrders.length ? currentNonNumericIndex + 1 : 0;
+        return nonNumericCouplets[nonNumericCoupletsOrders[nextOrderIndex]];
+      }
+    } else {
+      return nonNumericCouplets[nonNumericCoupletsOrders[0]];
+    }
+  }
+};
 
 const CoupletSelect = () => {
   const coupletsRef = useRef<HTMLElement>(null);
@@ -90,39 +132,11 @@ const CoupletSelect = () => {
       return;
     }
 
-    const nonNumericCoupletsOrders = Object.keys(nonNumericCouplets).map(Number);
+    if (e.key === '*' && nonNumericCouplets) {
+      const necessaryNonNumericSlide = getNecessaryNonNumericSlide(nonNumericCouplets, psalmData, currentSlide?.id);
 
-    if (e.key === '*' && nonNumericCoupletsOrders.length) {
-      if (nonNumericCoupletsOrders.length === 1) {
-        onUpdateSlide(nonNumericCouplets[nonNumericCoupletsOrders[0]]);
-      } else {
-        const currentCoupletOrder = psalmData.couplets.find(({ slide }) => slide?.id === currentSlide?.id)?.couplet
-          .initialOrder;
-
-        if (currentCoupletOrder !== undefined) {
-          const currentNonNumericIndex = nonNumericCoupletsOrders.indexOf(currentCoupletOrder);
-
-          if (currentNonNumericIndex === -1) {
-            const nextCoupletOrder = currentCoupletOrder + 1;
-            if (nonNumericCoupletsOrders.includes(nextCoupletOrder)) {
-              onUpdateSlide(nonNumericCouplets[nextCoupletOrder]);
-            } else {
-              const previousOrders = nonNumericCoupletsOrders.filter((i) => i < currentCoupletOrder);
-
-              if (previousOrders.length) {
-                onUpdateSlide(nonNumericCouplets[previousOrders[previousOrders.length - 1]]);
-              } else {
-                onUpdateSlide(nonNumericCouplets[nonNumericCoupletsOrders[0]]);
-              }
-            }
-          } else {
-            const nextOrderIndex =
-              currentNonNumericIndex + 1 < nonNumericCoupletsOrders.length ? currentNonNumericIndex + 1 : 0;
-            onUpdateSlide(nonNumericCouplets[nonNumericCoupletsOrders[nextOrderIndex]]);
-          }
-        } else {
-          onUpdateSlide(nonNumericCouplets[nonNumericCoupletsOrders[0]]);
-        }
+      if (necessaryNonNumericSlide) {
+        onUpdateSlide(necessaryNonNumericSlide);
       }
     } else {
       fastNumberSelectionKeyDown(e);
